321ed8cd497e68ccd834c35a7c58f8cf189438c5
CarlFlo06 <CarlFlo06>
2025-12-08 19:42:21 -0500

    version 1

---
+++ NaveEspacial/NaveEspacial/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NaveEspacial;
+using System.Drawing;
+
+//Console.WriteLine("Hello, World!");
+
+Ventana ventana = new Ventana(170,45,ConsoleColor.Black,new Point(5,3),new Point(150,43));
+ventana.DibujarMarco();
+Console.ReadKey();
+
+//Console.SetCursorPosition(5, 5);
+//Console.Write("Carlos");
+
+//Console.SetCursorPosition(20, 18);
+//Console.Write("Carlos");
+
+//Console.WriteLine("Ancho máximo:" + Console.LargestWindowWidth);
+//Console.WriteLine(("Altura máxima:"+Console.LargestWindowHeight)
+Console.ReadKey();
---
+++ NaveEspacial/NaveEspacial/Ventana.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaveEspacial
+{
+    internal class Ventana
+    {
+        public int Ancho { get; set; }
+        public int Altura { get; set; }
+        public Point LimiteSuperior { get; set; }
+        public Point LimiteInferior { get; set; }
+        public ConsoleColor Color { get; set; }
+        public Ventana(int ancho, int altura,ConsoleColor color, Point limiteSuperior,
+            Point limiteInferior)
+        {
+            Ancho = ancho;
+            Altura = altura;
+            Color = color;
+            LimiteSuperior = limiteSuperior;
+            LimiteInferior = limiteInferior;
+            Init();
+        }
+
+        private void Init()
+        {
+            Console.SetWindowSize(Ancho, Altura);
+            Console.Title = "Nave";
+            Console.CursorVisible = false;
+            Console.BackgroundColor = Color;
+            Console.Clear();
+        }
+
+        public void DibujarMarco()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            for(int i = LimiteSuperior.X; i <= LimiteInferior.X; i++)
+            {
+                Console.SetCursorPosition(i, LimiteSuperior.Y);
+                Console.Write("═");//ALT + 205
+                Console.SetCursorPosition(i, LimiteInferior.Y);
+                Console.Write("═");
+            }
+            for(int i = LimiteSuperior.Y; i <= LimiteInferior.Y; i++)
+            {
+                Console.SetCursorPosition(LimiteSuperior.X, i);
+                Console.Write("║");//ALT+ 186
+                Console.SetCursorPosition(LimiteInferior.X, i);
+                Console.Write("║");//ALT+ 186
+            }
+
+            Console.SetCursorPosition(LimiteSuperior.X, LimiteSuperior.Y);
+            Console.Write("╔");
+            Console.SetCursorPosition(LimiteSuperior.X, LimiteInferior.Y);
+            Console.Write("╚");
+
+            Console.SetCursorPosition(LimiteInferior.X, LimiteSuperior.Y);
+            Console.Write("╗"); //ALT+ 187
+            Console.SetCursorPosition(LimiteInferior.X, LimiteInferior.Y);
+            Console.Write("╝");// ALT + 188
+        }
+            
+    }
+}
